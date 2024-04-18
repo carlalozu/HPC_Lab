8001d59eb403ed6d30c0d25177de662bc9f3a630
Carla Lopez <calopez@student.ethz.ch>
2024-04-19 00:16:19 +0200

    create cartesian grid

--- project04/ghost/ghost.c
+++ project04/ghost/ghost.c
@@ -48,14 +48,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <stdbool.h>
 
 #define SUBDOMAIN 6
-#define DOMAINSIZE (SUBDOMAIN+2)
+#define DOMAINSIZE (SUBDOMAIN + 2)
 
 int main(int argc, char *argv[])
 {
     int rank, size, i, j, dims[2], periods[2], rank_top, rank_bottom, rank_left, rank_right;
-    double data[DOMAINSIZE*DOMAINSIZE];
+    double data[DOMAINSIZE * DOMAINSIZE];
     MPI_Request request;
     MPI_Status status;
     MPI_Comm comm_cart;
@@ -67,30 +68,59 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (size!=16) {
+    if (size != 16)
+    {
         printf("please run this with 16 processors\n");
         MPI_Finalize();
         exit(1);
     }
 
     // initialize the domain
-    for (i=0; i<DOMAINSIZE*DOMAINSIZE; i++) {
-        data[i]=rank;
+    for (i = 0; i < DOMAINSIZE * DOMAINSIZE; i++)
+    {
+        data[i] = rank;
     }
 
-    // TODO: set the dimensions of the processor grid and periodic boundaries in both dimensions
-    //dims[0]=
-    //dims[1]=
-    //periods[0]=
-    //periods[1]=
+    // set the dimensions of the processor grid and periodic boundaries in both dimensions
+    dims[0] = 0;
+    dims[1] = 0;
+    periods[0] = true;
+    periods[1] = true;
 
-    // TODO: Create a Cartesian communicator (4*4) with periodic boundaries (we do not allow
+    // Create a Cartesian communicator (4*4) with periodic boundaries (we do not allow
     // the reordering of ranks) and use it to find your neighboring
     // ranks in all dimensions in a cyclic manner.
+    int reorder = false;
     
-    // TODO: find your top/bottom/left/right neighbor using the new communicator, see MPI_Cart_shift()
+    MPI_Dims_create(size, 2, dims);
+    MPI_Comm new_communicator;
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &new_communicator);
+
+    enum DIRECTIONS
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    };
+    char *neighbours_names[4] = {"left", "right", "up", "down"};
+    int neighbours_ranks[4];
+
+    // Let consider dims[0] = X, so the shift tells us our left and right neighbours
+    MPI_Cart_shift(new_communicator, 1, 1, &neighbours_ranks[LEFT], &neighbours_ranks[RIGHT]);
+
+    // Let consider dims[1] = Y, so the shift tells us our up and down neighbours
+    MPI_Cart_shift(new_communicator, 0, 1, &neighbours_ranks[UP], &neighbours_ranks[DOWN]);
+
+    // find your top/bottom/left/right neighbor using the new communicator, see MPI_Cart_shift()
     // rank_top, rank_bottom
     // rank_left, rank_right
+    // Get my rank in the new communicator
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("[MPI process %d] I have a %s neighbour: process %d.\n", rank, neighbours_names[i], neighbours_ranks[i]);
+    }
 
     //  TODO: create derived datatype data_ghost, create a datatype for sending the column, see MPI_Type_vector() and MPI_Type_commit()
     // data_ghost
@@ -99,20 +129,22 @@ int main(int argc, char *argv[])
     //  use MPI_Irecv(), MPI_Send(), MPI_Wait() or other viable alternatives
 
     //  to the top
-    
-    //  to the bottom
-    
-    //  to the left
-    
-    //  to the right
-    
 
-    if (rank==9) {
+    //  to the bottom
+
+    //  to the left
+
+    //  to the right
+
+    if (rank == 9)
+    {
         printf("data of rank 9 after communication\n");
-        for (j=0; j<DOMAINSIZE; j++) {
-            for (i=0; i<DOMAINSIZE; i++) {
-                printf("%.1f ", data[i+j*DOMAINSIZE]);
-                printf("%4.1f ", data[i+j*DOMAINSIZE]);
+        for (j = 0; j < DOMAINSIZE; j++)
+        {
+            for (i = 0; i < DOMAINSIZE; i++)
+            {
+                printf("%.1f ", data[i + j * DOMAINSIZE]);
+                printf("%4.1f ", data[i + j * DOMAINSIZE]);
             }
             printf("\n");
         }
