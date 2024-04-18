a03870463e6e40200a83f5764ef53845c1aed35c
Carla Lopez <calopez@student.ethz.ch>
2024-04-18 23:33:30 +0200

    communication in a ring

--- project04/ring/ring_sum.c
+++ project04/ring/ring_sum.c
@@ -1,16 +1,52 @@
 #include <mpi.h> // MPI
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
   // Initialize MPI, get size and rank
   int size, rank;
+  char pname[MPI_MAX_PROCESSOR_NAME];
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Status status;
 
   // IMPLEMENT: Ring sum algorithm
-  int sum = 0; // initialize sum
+  int rank_dest, rank_source;
+  int out_msg, in_msg;
+
+  int sum = 0, i; // initialize sum
+  for (i = 0; i < size; i++)
+  {
+
+    rank_source = rank - 1;
+    if (rank == 0)
+      rank_source = size - 1;
+
+    rank_dest = rank + 1;
+    if (rank == size - 1)
+      rank_dest = 0;
+
+    if (i == 0)
+    {
+      out_msg = rank;
+    }
+
+    if ((rank % 2) != 0)
+    {
+      MPI_Recv(&in_msg, 1, MPI_INT, rank_source, 0, MPI_COMM_WORLD, &status);
+      MPI_Send(&out_msg, 1, MPI_INT, rank_dest, 0, MPI_COMM_WORLD);
+    }
+    else
+    {
+      MPI_Send(&out_msg, 1, MPI_INT, rank_dest, 0, MPI_COMM_WORLD);
+      MPI_Recv(&in_msg, 1, MPI_INT, rank_source, 0, MPI_COMM_WORLD, &status);
+    }
+    sum = sum + in_msg;
+    out_msg = in_msg;
+  }
+
   printf("Process %i: Sum = %i\n", rank, sum);
 
   // Finalize MPI
